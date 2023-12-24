b1e06b58ffd725986c714553f023af6205b308ef
Anton Kolotusha <qq102096@gmail.com>
2023-12-24 23:14:05 +0100

    Solved

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,12 +3,14 @@ package com.epam.mjc.io;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class FileReader {
+    private static final Logger LOGGER = Logger.getLogger(FileReader.class.getName());
 
     public Profile getDataFromFile(File file) {
         Map<String, String> profileMap = new HashMap<>();
+
         try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){
             String line;
             while((line = br.readLine()) != null){
@@ -19,7 +21,7 @@ public class FileReader {
             }
         }
         catch (IOException e){
-            System.out.println("fafa");
+            LOGGER.log(Level.SEVERE, "Error occurred while reading the file.", e);
         }
         return new Profile(profileMap .get("Name"), Integer.parseInt(profileMap .get("Age")), profileMap .get("Email"), Long.parseLong(profileMap .get("Phone")));
     }
