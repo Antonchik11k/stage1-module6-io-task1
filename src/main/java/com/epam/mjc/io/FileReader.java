372f7a02a562b0169a09677a45f0b47d6f7cc81b
Anton Kolotusha <qq102096@gmail.com>
2023-12-24 23:09:17 +0100

    Solved

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -8,19 +8,19 @@ import java.util.Map;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        Map<String, String> profile_info = new HashMap<>();
+        Map<String, String> profileMap = new HashMap<>();
         try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){
             String line;
             while((line = br.readLine()) != null){
                 var parts = line.split(": ");
                 if(parts.length == 2){
-                    profile_info.put(parts[0].trim(), parts[1].trim());
+                    profileMap .put(parts[0].trim(), parts[1].trim());
                 }
             }
         }
         catch (IOException e){
-
+            System.out.println("fafa");
         }
-        return new Profile(profile_info.get("Name"), Integer.parseInt(profile_info.get("Age")), profile_info.get("Email"), Long.parseLong(profile_info.get("Phone")));
+        return new Profile(profileMap .get("Name"), Integer.parseInt(profileMap .get("Age")), profileMap .get("Email"), Long.parseLong(profileMap .get("Phone")));
     }
 }
