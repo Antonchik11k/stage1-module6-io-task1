c0e5a346c0cc82d2104f0efaa2a8fa3dd87d88c5
Anton Kolotusha <qq102096@gmail.com>
2023-12-24 23:05:40 +0100

    Solved

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,26 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> profile_info = new HashMap<>();
+        try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){
+            String line;
+            while((line = br.readLine()) != null){
+                var parts = line.split(": ");
+                if(parts.length == 2){
+                    profile_info.put(parts[0].trim(), parts[1].trim());
+                }
+            }
+        }
+        catch (IOException e){
+
+        }
+        return new Profile(profile_info.get("Name"), Integer.parseInt(profile_info.get("Age")), profile_info.get("Email"), Long.parseLong(profile_info.get("Phone")));
     }
 }
